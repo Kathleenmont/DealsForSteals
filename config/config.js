99bd2a3b8d5887df0787e9d4421715095bf6bd0e
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2019-05-26 10:18:25 -0400

    test-1

--- config/config.js
+++ config/config.js
@@ -1,17 +1,10 @@
 module.exports = {
   development: {
     username: "root",
-<<<<<<< HEAD
-    password: process.env.DB_password,
-    database: "exampledb",
-    host: "localhost",
-    port:3306,
-=======
     password: process.env.DB_PASSWORD,
     database: "dealsdb",
     host: "localhost",
     port: process.env.DB_PORT,
->>>>>>> c0f0829b98326935b866e952afa4043ba32359df
     dialect: "mysql"
   },
   test: {
@@ -23,15 +16,8 @@ module.exports = {
     logging: false
   },
   production: {
-<<<<<<< HEAD
-    use_env_variable: "JAWSDB_URL",
-    dialect: "mysql"
-  }
-}
-=======
     // eslint-disable-next-line camelcase
     use_env_variable: "JAWSDB_URL",
     dialect: "mysql"
   }
 };
->>>>>>> c0f0829b98326935b866e952afa4043ba32359df
