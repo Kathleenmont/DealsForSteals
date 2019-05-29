7395654f4cbfeaa1bfdcb43ea4750968648c4076
Kathleen Montesano <kathleen.m.montesano@gmail.com>
2019-05-29 13:26:32 -0400

    passport

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,14 @@
 var db = require("../models");
 
+// passport added _____________________________
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+
+
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
@@ -17,13 +26,33 @@ module.exports = function(app) {
     res.render("homepage");
   });
 
-  app.get("/uploads", function(req, res) {
-    res.render("uploads");
+  app.get("/newpost", function(req, res) {
+    // PASPORT If STATEMENT - if the user already has an account send them to the search page
+    if (req.user) {
+      res.render("newpost");
+    }
+    res.render("signup");
   });
 
-  app.get("/final/", function(req, res) {
+  app.get("/final", function(req, res) {
     res.render("thank_you_page");
   });
+
+  // PASSOPRT ADDED ______________________
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/homepage");
+    }
+    res.render("login");
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  // app.get("/newpost", isAuthenticated, function(req, res) {
+  //   res.render("newpost");
+  // });
+  // ______________________________________
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
@@ -34,7 +63,7 @@ module.exports = function(app) {
     });
   });
   // test search page
-  app.get("/search/", function(req, res) {
+  app.get("/search", function(req, res) {
     db.Post.findAll({}).then(function(dbPost) {
       // console.log(dbPost);
       res.render("all_deals", {
