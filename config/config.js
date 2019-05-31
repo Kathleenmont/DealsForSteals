f6969b2ea1c261aedf1a99c313717aa24094bf9b
Kathleen Montesano <kathleen.m.montesano@gmail.com>
2019-05-31 09:40:05 -0400

    heroku

--- config/config.js
+++ config/config.js
@@ -5,7 +5,18 @@ module.exports = {
     database: "dealsdb",
     host: "localhost",
     port: process.env.DB_PORT,
-    dialect: "mysql"
+    dialect: "mysql",
+    // eslint-disable-next-line camelcase
+    cloud_name: "CLOUD_NAME",
+    // eslint-disable-next-line camelcase
+    api_key: "CLOUD_API_KEY",
+    // eslint-disable-next-line camelcase
+    api_secret: "CLOUD_API_SECRET",
+    // eslint-disable-next-line camelcase
+    enhance_image_tag: true,
+    // eslint-disable-next-line camelcase
+    static_image_support: true
+    // eslint-disable-next-line camelcase
   },
   test: {
     username: "root",
@@ -13,9 +24,30 @@ module.exports = {
     database: "testdb",
     host: "localhost",
     dialect: "mysql",
-    logging: false
+    logging: false,
+    // eslint-disable-next-line camelcase
+    cloud_name: "CLOUD_NAME",
+    // eslint-disable-next-line camelcase
+    api_key: "CLOUD_API_KEY",
+    // eslint-disable-next-line camelcase
+    api_secret: "CLOUD_API_SECRET",
+    // eslint-disable-next-line camelcase
+    enhance_image_tag: true,
+    // eslint-disable-next-line camelcase
+    static_image_support: true
+    // eslint-disable-next-line camelcase
   },
   production: {
+    // eslint-disable-next-line camelcase
+    cloud_name: "CLOUD_NAME",
+    // eslint-disable-next-line camelcase
+    api_key: "CLOUD_API_KEY",
+    // eslint-disable-next-line camelcase
+    api_secret: "CLOUD_API_SECRET",
+    // eslint-disable-next-line camelcase
+    enhance_image_tag: true,
+    // eslint-disable-next-line camelcase
+    static_image_support: true,
     // eslint-disable-next-line camelcase
     use_env_variable: "JAWSDB_URL",
     dialect: "mysql"
